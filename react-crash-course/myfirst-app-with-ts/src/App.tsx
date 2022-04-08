4ebf3d1f34a73739f96ae08ff36ecdec7d29ad2b
timi-o3 <86927745+timi-o3@users.noreply.github.com>
2022-04-09 00:27:37 +0100

    helloworld6

---
+++ react-crash-course/myfirst-app-with-ts/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HelloWorld from './components/HelloWorld';
+const App: React.FC = () => {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+export default App;
---
+++ react-crash-course/myfirst-app-with-ts/src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// strict mode side is used to control the app so if there any errors it would find it.
+reportWebVitals();
